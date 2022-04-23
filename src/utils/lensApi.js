9f2f4e01c488d07c9bab25d474ae5370f460bc50
İzzet Emre Demir <izzetemredemir@gmail.com>
2022-04-23 11:28:29 +0200

    lensApi.js: LensProtocol implemented

--- src/utils/lensApi.js
+++ src/utils/lensApi.js
@@ -10,3 +10,52 @@ export const queryExample = async () => {
   const response = await urqlClient.query(pingQuery).toPromise();
   console.log('Lens example data: ', response)
 }
+
+const GET_PING = `
+  query {
+    ping
+  }
+`
+
+export const ping = () => {
+   return apolloClient.query({
+    query: gql(GET_PING),
+  })
+}
+
+const GET_USERS_NFTS = `
+  query($request: NFTsRequest!) {
+    nfts(request: $request) {
+      items {
+        contractName
+        contractAddress
+        symbol
+        tokenId
+        owners {
+          amount
+          address
+        }
+        name
+        description
+        contentURI
+        originalContent {
+          uri
+          metaType
+        }
+        chainId
+        collectionName
+        ercType
+      }
+    pageInfo {
+        prev
+        next
+        totalCount
+    }
+  }
+}
+`
+export const GetNFT = () => {
+    return apolloClient.query({
+     query: gql(GET_USERS_NFTS),
+   })
+}
