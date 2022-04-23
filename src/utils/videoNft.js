b9c0930258ee7fad195526cd37f39fed97b8b11b
İzzet Emre Demir <izzetemredemir@gmail.com>
2022-04-23 19:15:48 +0200

    alchemy web3 added

--- src/utils/videoNft.js
+++ src/utils/videoNft.js
@@ -1,9 +1,4 @@
-import { videonft } from '@livepeer/video-nft'
-
-// To upload videos from the filesystem
-const uploader = new videonft.minter.Uploader();
-// To process videos and export to IPFS
-const vodApi = new videonft.minter.Api({ auth: { apiKey } });
-// To mint the NFT from the IPFS files
-const { chainId } = ethereum
-const web3 = new videonft.minter.Web3({ ethereum, chainId });
+require('dotenv').config();
+const API_URL = process.env.API_URL;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
