770273ebcdc7421f601f795351f3c565e9d7c93b
İzzet Emre Demir <izzetemredemir@gmail.com>
2022-04-24 04:33:33 +0200

    postComment added

--- src/utils/comments.js
+++ src/utils/comments.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
-export const getComments = async() => {
+
+
+export const getComments = async(id) => {
     let resp;
-    axios.get().then(tresp => {
+    url = "https://ethamsterdam.herokuapp.com/streams/"+id+"/comments";
+    axios.get(url).then(tresp => {
         console.log(tresp);
       
   tresp = resp;
@@ -11,3 +14,26 @@ export const getComments = async() => {
 }
    
 
+
+
+export const postComment = async(id,message,token) => {
+    let resp;
+    url = "https://ethamsterdam.herokuapp.com/streams/"+id+"/comments";
+    header= "Authorization: Bearer"+token;
+    axios({
+        method: 'post',
+        url: '/login',
+        data: {
+            body: message
+        
+        },
+        headers: {header},
+      }).then(tresp => {
+        console.log(tresp);
+      
+  tresp = resp;
+  });
+
+  return resp;
+}
+   
