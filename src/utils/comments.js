17a4ff60df56db0b9be3590d68842224b5d4852d
İzzet Emre Demir <izzetemredemir@gmail.com>
2022-04-24 04:25:52 +0200

    getComments added

---
+++ src/utils/comments.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+export const getComments = async() => {
+    let resp;
+    axios.get().then(tresp => {
+        console.log(tresp);
+      
+  tresp = resp;
+  });
+
+  return resp;
+}
+   
+
