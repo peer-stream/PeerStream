ed810ec97bba1dd99d8eda3493e44e73e384e965
Ahmet Aygün <me@ahmetaygun.net>
2022-04-24 03:12:12 +0200

    integrate walletconnect, coinbase. use shared context and hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,61 @@
 import React  from 'react';
-import '../App.css';
 import { useEffect, useState } from "react";
-import { connectWallet, getCurrentWalletConnected, authorizeWallet } from "../utils/wallet.js";
-import { setAuthToCache, getAuthCache } from '../utils/cacheAuth';
+import axios from 'axios'
+import { useWeb3 } from '../utils/web3ModalContext';
+import '../App.css';
 
 const Header = (props) => {
+  const web3 = useWeb3();
+  const [connected, setConnected] = useState(false);
 
-  const [walletAddress, setWallet] = useState("");
-  const [status, setStatus] = useState("");
-
-  function addWalletListener() {
-    if (window.ethereum) {
-      window.ethereum.on("accountsChanged", (accounts) => {
-        if (accounts.length > 0) {
-          setWallet(accounts[0]);
-          setStatus("");
-        } else {
-          setWallet("");
-          setStatus("");
-        }
-      });
-    } else {
-      setStatus(
-        "Metamask is required"
-      );
-    }
-  }  
   useEffect(() => {
-    const wallet = async () => {
-      return await getCurrentWalletConnected();
+    if (connected) {
+      const accessToken = localStorage.getItem('access_token');
+      const accessTokenExpiry = Number(localStorage.getItem('access_token_expires_at'));
+      const now = Date.now();
+      if (!accessToken || accessToken.length === 0 || now > accessTokenExpiry) {
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('access_token_expires_at');
+        signIn();
+      } else {
+        console.log('token exists and not expired')
+      }
     }
-    const authorize = async () => {
-      const {token} = await authorizeWallet(walletAddress);
-      setAuthToCache(token);
-    }
-    const {address, status} = wallet().catch(error => console.error);
-    setWallet(address);
-    setStatus(status);
-    addWalletListener(); 
-    authorize();
-  }, []);
-  
-  const connectWalletPressed = async () => {
-    const walletResponse = await connectWallet();
-    setStatus(walletResponse.status);
-    setWallet(walletResponse.address);
-    const {token} = await authorizeWallet(walletAddress);
-    setAuthToCache(token);
-  };
+  }, [connected]);
 
+  useEffect(() => {
+    if (web3.account) {
+      setConnected(true);
+    } else {
+      setConnected(false);
+    }
+  }, [web3.account]);
+
+  const connectToWallet = async () => {
+    if (connected) {
+      await web3.clearCachedProvider();
+      setConnected(false);
+    } else {
+      try {
+        await web3.connect();
+        setConnected(true);
+      } catch (err) {
+        console.log(err);
+        setConnected(false);
+      }
+    }
+  }
+
+  const signIn = async () => {
+    const nonceRequest = await axios.post('https://ethamsterdam.herokuapp.com/auth/nonce', { address: web3.account });
+    const signature = await web3.signer?.signMessage(nonceRequest.data.message_to_sign);
+    const accessTokenRequest = await axios.post('https://ethamsterdam.herokuapp.com/auth/token', {
+      address: web3.account,
+      signature
+    });
+    localStorage.setItem('access_token', accessTokenRequest.data.token);
+    localStorage.setItem('access_token_expires_at', accessTokenRequest.data.expires_at);
+  }
 
   return (
     <div className="border-gradient border-gradient-purple">
@@ -58,17 +65,16 @@ const Header = (props) => {
         </div>
         <div className="right-side">
         <a href="/mint">Mint Video NFT</a>
-        {/* <a href="#">Docs</a>
-        <a href="#">About</a> */}
-        <p className='connect-wallet' onClick={ ()=>connectWalletPressed() }>
-        {walletAddress?.length > 0 ? (
-          "Connected: " +
-          String(walletAddress).substring(0, 6) +
-          "..." +
-          String(walletAddress).substring(38)
-        ) : (
-          <span>Connect Wallet</span>
-        )}</p>
+        <p className='connect-wallet' onClick={connectToWallet}>
+          {connected ? (
+            "Connected: " +
+            String(web3.account).substring(0, 6) +
+            "..." +
+            String(web3.account).substring(38)
+          ) : (
+            <span>Connect Wallet</span>
+          )}
+        </p>
         </div>
       </nav>
     </div>
